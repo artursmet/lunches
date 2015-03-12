e4feb4519315d4cf40f5ea19a474695d44d60488
Artur Smęt <artur.smet@gmail.com>
2015-03-12 22:18:09 +0100

    Add redis cache

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 # coding: utf-8
+from __future__ import unicode_literals
+
+import datetime
 import json
 import os
+import redis
+
 from flask import Flask
 from flask import render_template
+
 from parser import get_last_entry
 
 app = Flask(__name__)
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis = redis.from_url(redis_url)
+CACHE_TIME = datetime.timedelta(hours=1)
 
 
 @app.route('/')
@@ -14,9 +23,18 @@ def index():
     restaurants = json.loads(open('restaurants.json').read())
     for restaurant_name in restaurants:
         place_data = restaurants[restaurant_name]
+        place_id = place_data['id']
+        cache_key = 'last-entry-%s' % (place_id,)
+        last_entry = redis.get(cache_key)
+        if last_entry:
+            last_entry = last_entry.decode('utf-8')
+        else:
+            last_entry = get_last_entry(place_id)
+            redis.setex(name=cache_key, time=CACHE_TIME, value=last_entry)
+        
         last_entries.append(
             {'name': restaurant_name, 'url': place_data['url'],
-             'last_entry': get_last_entry(place_data['id'])}
+             'last_entry': last_entry}
         )
     return render_template('index.html', last_entries=last_entries)
 
