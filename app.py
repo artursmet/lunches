e6b2157880e16b992587b37901813bc1d30a7572
Artur Smęt <artur.smet@mirumee.com>
2015-03-12 17:05:18 +0100

    Get restaurants from JSON file

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-#coding: utf-8
+# coding: utf-8
+import json
 from flask import Flask
 from flask import render_template
+from parser import get_last_entry
 
 app = Flask(__name__)
 
-RESTAURANTS = [
-	{
-		'name': u'Ślepy Zaułek',
-		'menu': u'Lunch od 12 do 15. 20 zł za zestaw'
-	}
-]
 
 @app.route('/')
 def index():
-	return render_template('index.html', restaurants=RESTAURANTS)
+    last_entries = []
+    restaurants = json.loads(open('restaurants.json').read())
+    for restaurant_name in restaurants:
+        place_data = restaurants[restaurant_name]
+        last_entries.append(
+            {'name': restaurant_name, 'url': place_data['url'],
+             'last_entry': get_last_entry(place_data['id'])}
+        )
+    return render_template('index.html', last_entries=last_entries)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
